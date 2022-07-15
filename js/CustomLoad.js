d19cdea37272f6aa3d612aa3382471576e0b9fee
Handsome Boy <48283948+lolamchan@users.noreply.github.com>
2022-07-15 19:03:37 +0800

    Update CustomLoad.js

--- js/CustomLoad.js
+++ js/CustomLoad.js
@@ -1,4 +1,5 @@
-function Customload() {
+//This is for filling my skill bars
+function CustomloadFillBar() {
   var reveals = document.querySelectorAll(".CustomLeft");
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
@@ -13,4 +14,19 @@ function Customload() {
     }
   }
 }
-window.addEventListener("scroll", Customload);
+window.addEventListener("scroll", CustomloadFillBar);
+//This is for animating my images
+function CustomloadImageUp() {
+  var reveals = document.querySelectorAll(".CustomUp");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var offset = 0;
+    if (elementTop < windowHeight + offset) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", CustomloadImageUp);
